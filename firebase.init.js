89a5b005767b88c3e03db35a4e963b9d3897497a
vybe2025 <suporte.vybe@gmail.com>
2025-03-06 09:46:22 +0100

    Add files via upload

---
+++ firebase.init.js
@@ -0,0 +1,26 @@
+// Aguarda o carregamento do Firebase
+document.addEventListener("DOMContentLoaded", function() {
+  // Configuração do Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyDF9jRh9F5WBANaKeAlmeCJUt0urIuHEnI",
+    authDomain: "database-vybe.firebaseapp.com",
+    projectId: "database-vybe",
+    storageBucket: "database-vybe.appspot.com",
+    messagingSenderId: "1095806602541",
+    appId: "1:1095806602541:web:482061f30a0c1c9e1057a4",
+    measurementId: "G-BTJMQXCWZV"
+  };
+  
+  // Inicializa o Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Verifica se a inicialização foi bem-sucedida
+  if (firebase.apps.length) {
+    console.log("Firebase inicializado com sucesso");
+  } else {
+    console.error("Erro ao inicializar Firebase");
+  }
+  
+  // Inicializa Firestore
+  const db = firebase.firestore();
+});
